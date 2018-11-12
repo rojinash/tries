72132e5006842be750e5135826f3fbec0d31393b
rojinash <sharmaro@grinnell.edu>
2018-11-11 18:02:17 -0600

    Add files via upload

---
+++ tries/src/tries/TrieNode.java
@@ -0,0 +1,14 @@
+package tries;
+import java.util.HashMap;
+public class TrieNode {
+	private HashMap<Character, TrieNode> children = new HashMap<>();
+    public int size;
+
+    public void putChildIfAbsent(char ch) {
+        children.putIfAbsent(ch, new TrieNode());
+    }
+
+    public TrieNode getChild(char ch) {
+        return children.get(ch);
+    }
+}
